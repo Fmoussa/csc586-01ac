89dbb6f159a18fcf5626d2a457a9185a1afa2c16
Steve Onorato <steveonoratojr@gmail.com>
2022-07-22 19:51:12 -0400

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,3 @@
+# Check to see if there are any changes to /var/webserver_monitor/unauthorized.log. 
+#  If there are new entries, an email should be sent to the admin with the content of these new entries. 
+#  Otherwise, the email simply says "No unauthorized access."
