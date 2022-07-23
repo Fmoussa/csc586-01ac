0a4062dfb74eb7099fa5de3eebd8e7c5d40e24a3
Steve Onorato <steveonoratojr@gmail.com>
2022-07-23 16:28:11 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -5,7 +5,7 @@ set -x
 #  and $country are read in as variables. Each line is checked for a non-null $ip_address, and then 
 #  is written to: /share/log/unauthorized.log. This ensures only unauthorized/failed ssh activity is
 #  recorded.
-sudo grep -i -E "invalid|failed" /var/log/auth.log | while read -r line ; do
+sudo grep -i -E "invalid|fail" /var/log/auth.log | while read -r line ; do
   date=$(echo $line | grep -i -o -E "^[a-z]*\s[0-9]*\s")
   ip_address=$(echo $line | grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
   country=$(curl -s ipinfo.io/$ip_address | grep -o -P '(?<="country": )[^ ]*' | grep -io "[a-z]*")
