7e1938b6b8c216c76e6c10e3c30f3cdd1cdeaa87
Steve Onorato <steveonoratojr@gmail.com>
2022-07-23 20:06:23 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -9,7 +9,7 @@ sudo chown nobody:nogroup /share/log
 echo "/share/log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 sudo systemctl restart nfs-kernel-server
 
-
+sudo chmod 755 /local/repository/scan.sh
 
 crontab -l > tempcron
 echo "*/5 * * * * /local/repository/scan.sh" >> tempcron
