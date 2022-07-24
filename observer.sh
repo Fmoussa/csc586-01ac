5ecb2f6d72764c71b7e2f90ad27e386d69003b3a
Steve Onorato <steveonoratojr@gmail.com>
2022-07-24 12:01:17 -0400

    Update observer.sh

--- observer.sh
+++ observer.sh
@@ -5,3 +5,7 @@ sudo apt install -y nfs-common
 sudo mkdir webserver_log
 sleep 2m
 sudo mount 192.168.1.1:/share/log webserver_log/
+
+sudo chmod 755 /local/repository/monitor.sh
+
+(crontab -l 2>/dev/null; echo "0 * * * * /local/repository/monitor.sh") | crontab -
