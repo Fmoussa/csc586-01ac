5a99c97cfc8d6608f23730b86aa1c1c785c912fc
Steve Onorato <steveonoratojr@gmail.com>
2022-07-24 11:57:19 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+set -x
+
 # Check to see if there are any changes to /var/webserver_monitor/unauthorized.log. 
 #  If there are new entries, an email should be sent to the admin with the content of these new entries. 
 #  Otherwise, the email simply says "No unauthorized access."
+
+isdifflog='diff /webserver_log/unauthorized.log /webserver_log/temp.log'
+
+if [[ $isdifflog -eq 0 ]]
+then
+  echo "No unauthorized access" | mail -s "No New Unauthorized Access" so749257@wcupa.edu
+else
+  echo -n "Unauthorized access report: $isdifflog" | mail -s "New Unauthorized Access" so749257@wcupa.edu
+fi
+
+sudo bash -c "cat /webserver_log/unauthorized.log > /webserver_log/temp.log"
