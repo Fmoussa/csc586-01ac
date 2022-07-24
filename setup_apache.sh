4f9555a9fc199ac6a35ab29383388498b3b2e244
Steve Onorato <steveonoratojr@gmail.com>
2022-07-23 20:51:02 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -11,10 +11,10 @@ sudo systemctl restart nfs-kernel-server
 
 sudo chmod 755 /local/repository/scan.sh
 
-crontab -l > tempcron
-echo "*/5 * * * * su so749257 -c /local/repository/scan.sh" >> tempcron
-crontab tempcron
-rm tempcron
+#crontab -l > tempcron
+#echo "*/5 * * * * su so749257 -c /local/repository/scan.sh" >> tempcron
+#crontab tempcron
+#rm tempcron
 
 #crontab -l | { echo "*/5 * * * * /local/repository/scan.sh"; } | crontab -
 #(crontab -l ; echo "*/5 * * * * /local/repository/scan.sh) | crontab -
