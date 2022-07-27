ece66c7f6d42cc2637609e12ac293794f7d7a0bd
Steve Onorato <steveonoratojr@gmail.com>
2022-07-26 20:28:08 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -5,13 +5,21 @@ set -x
 #  and $country are read in as variables. Each line is checked for a non-null $ip_address, and then 
 #  is written to: /share/log/unauthorized.log. This ensures only unauthorized/failed ssh activity is
 #  recorded.
-sudo grep -i -E "invalid|fail" /var/log/auth.log | while read -r line ; do
-  date=$(echo $line | grep -i -o -E "^[a-z]*\s[0-9]*\s")
-  ip_address=$(echo $line | grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
-  country=$(curl -s ipinfo.io/$ip_address | grep -o -P '(?<="country": )[^ ]*' | grep -io "[a-z]*")
+
+isdifflog=$(diff /var/log/auth.log /var/log/temp.log)
+
+if [[ $isdifflog -ne 0 ]]
+then
+  sudo grep -i -E "invalid|fail" $isdifflog | while read -r line ; do
+    date=$(echo $line | grep -i -o -E "^[a-z]*\s[0-9]*\s")
+    ip_address=$(echo $line | grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+    country=$(curl -s ipapi.co/$ip_address/country_name/)
   
-  if [ -n "$ip_address" ]
-  then
-    echo "$ip_address $country $date" | sudo tee /share/log/unauthorized.log
-  fi
-done 
+    if [ -n "$ip_address" ]
+    then
+      echo "$ip_address $country $date" | sudo tee -a /share/log/unauthorized.log
+    fi
+  done 
+fi
+
+sudo bash -c "cat /var/log/auth.log > /var/log/temp.log"
