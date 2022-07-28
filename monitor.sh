27f28b7a00ef6fa17faf370e9f859a00a41601f3
Steve Onorato <steveonoratojr@gmail.com>
2022-07-28 16:54:40 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,12 +5,14 @@ set -x
 #  If there are new entries, an email should be sent to the admin with the content of these new entries. 
 #  Otherwise, the email simply says "No unauthorized access."
 
-isdifflog=$(sudo diff /webserver_log/unauthorized.log /webserver_log/temp.log)
+echo -n '' | sudo tee -a /var/log/atemp.log
+
+isdifflog=$(sudo diff /webserver_log/unauthorized.log /webserver_log/atemp.log)
 
 if [[ $isdifflog -eq 0 ]]
 then
   echo "No unauthorized access" | mail -s "No New Unauthorized Access" so749257@wcupa.edu
 else
   echo -n "Unauthorized access report: $isdifflog" | mail -s "New Unauthorized Access" so749257@wcupa.edu
-  sudo cat /webserver_log/unauthorized.log | sudo tee /webserver_log/temp.log"
+  sudo cat /webserver_log/unauthorized.log | sudo tee /webserver_log/atemp.log"
 fi
