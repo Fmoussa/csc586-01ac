0ab6d69c9b1480b7847c1f4b1aa12971359b2b86
Steve Onorato <steveonoratojr@gmail.com>
2022-07-28 21:29:49 -0400

    Update ldap.sh

--- ldap.sh
+++ ldap.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 set -x
+
+sudo apt update
+sudo apt install -y nfs-kernel-server
+sudo mkdir /nfs/home
+sudo chown nobody:nogroup /nfs/home
+echo "/nfs/home 192.168.1.1(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/nfs/home 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+sudo systemctl restart nfs-kernel-server
