49e77092e3080751ba716faafdf25d27a13d6726
Steve Onorato <steveonoratojr@gmail.com>
2022-07-28 21:34:22 -0400

    Update observer.sh

--- observer.sh
+++ observer.sh
@@ -3,8 +3,11 @@ set -x
 
 sudo apt install -y nfs-common
 sudo mkdir webserver_log
+sudo mkdir /nfs/home
 sleep 2m
 sudo mount 192.168.1.1:/share/log webserver_log/
+sleep 2m
+sudo mount 192.168.1.3:/nfs/home nfs/home/
 
 sudo chmod 755 /local/repository/monitor.sh
 
