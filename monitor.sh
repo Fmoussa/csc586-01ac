209ff5c54255cb208efd1cf9fef4633893cc3983
Steve Onorato <steveonoratojr@gmail.com>
2022-07-30 11:09:00 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-# Check to see if there are any changes to /var/webserver_monitor/unauthorized.log. 
+# monitor.sh checks to see if there are any changes to /var/webserver_monitor/unauthorized.log. 
 #  If there are new entries, an email should be sent to the admin with the content of these new entries. 
 #  Otherwise, the email simply says "No unauthorized access."
 
